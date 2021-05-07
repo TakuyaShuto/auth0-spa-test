54816ee83519ba4291a208c261b9f25f15025ef1
takuya.shuto <takuya.shuto@toppan.co.jp>
2021-05-07 18:50:30 +0900

    Auth0SPAテスト用

---
+++ Sample-01/src/app/pages/external-api/external-api.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { AuthClientConfig } from '@auth0/auth0-angular';
+import { ApiService } from 'src/app/api.service';
+
+@Component({
+  selector: 'app-external-api',
+  templateUrl: './external-api.component.html',
+  styleUrls: ['./external-api.component.css'],
+})
+export class ExternalApiComponent {
+  responseJson: string;
+  audience = this.configFactory.get()?.audience;
+  hasApiError = false;
+
+  constructor(
+    private api: ApiService,
+    private configFactory: AuthClientConfig
+  ) {}
+
+  pingApi() {
+    this.api.ping$().subscribe({
+      next: (res) => {
+        this.hasApiError = false;
+        this.responseJson = JSON.stringify(res, null, 2).trim();
+      },
+      error: () => this.hasApiError = true,
+    });
+  }
+
+  test() {
+    this.api.test$().subscribe(res => {
+      this.hasApiError = false;
+      this.responseJson = JSON.stringify(res, null, 2).trim();
+    }, () => this.hasApiError = true);
+  }
+}
